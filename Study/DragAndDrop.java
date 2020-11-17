cf6ad95a89ea534d482eff636b023f4a0333229a
Alice Kaplan <73661389+XMRDland@users.noreply.github.com>
2020-11-17 19:59:42 +0300

    removed unnecessary comments

--- Study/DragAndDrop.java
+++ Study/DragAndDrop.java
@@ -7,9 +7,4 @@ public class DragAndDrop extends Task implements Autochecked {
         super(taskNum, taskText);
         this.answerVariants = new String[][] {{"a", "b"}, {"c", "d"}};
     }
-
-/*    public DragAndDrop(int taskNum, String taskText,String[][] answerVariants) {
-        super(taskNum, taskText);
-        this.answerVariants = answerVariants;
-    }*/
 }
