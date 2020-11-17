a339504c1f7d588e2f0996f9bd8e6b663e75d9a4
Alice Kaplan <73661389+XMRDland@users.noreply.github.com>
2020-11-17 19:58:57 +0300

    removed unnecessary comments

--- Study/Test.java
+++ Study/Test.java
@@ -7,9 +7,4 @@ public class Test extends Task implements Autochecked{
         super(taskNum, taskText);
         this.answers = new String[] {"a", "b", "c"};
     }
-
- /*   public Test(int taskNum, String taskText, String[] answers) {
-        super(taskNum, taskText);
-        this.answers = answers;
-    }*/
 }
